c7340a852d9382d560ac43c0eff7672223a4c206
StewieC <makokha.stewart21@students.dkut.ac.ke>
2025-04-26 12:39:42 +0300

    Update login and password reset templates; add URL routing for password reset functionality

--- easyM/urls.py
+++ easyM/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('contributions.urls')),
     path('accounts/', include('accounts.urls')),
+    path('password-reset/', include('password_reset.urls')),
 ]
